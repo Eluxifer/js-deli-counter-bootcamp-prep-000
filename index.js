66b6813deef2bd806cc814dc89257af28ee6dac1
Jeison Rodriguez <jeisonrodriguez94@outlook.com>
2018-12-04 19:44:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  return `Welcome, You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine){
+
   while (katzDeliLine.length > 0){
     return `Currently serving ${katzDeliLine.shift()}.`
   }

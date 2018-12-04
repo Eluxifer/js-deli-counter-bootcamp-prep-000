29f236dc44d397484167122ea4af1fa23d0929cd
Jeison Rodriguez <jeisonrodriguez94@outlook.com>
2018-12-04 18:51:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine > 0){
+    return "Currently serving Steven."
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
